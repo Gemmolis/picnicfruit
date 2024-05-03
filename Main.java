bb88b09b21f21f2e6057d9e4e49c0dc4f9704823
Nastya <142719774+Gemmolis@users.noreply.github.com>
2024-05-03 20:26:07 +0300

    Initial commit Ready

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Проанализировав предложенный файл, могу сообщить, что:   ");
+        operations.counterWord();
+        operations.wordLengthMax();
+        operations.frequencyWord();
+
+    }
+}
---
+++ operations.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+public class operations {
+    public static void counterWord(){
+        try{
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            int countWord = 0;
+
+            while (scanner.hasNext()){
+                scanner.next();
+                countWord++;
+            }
+            System.out.println("Всего слов в файле ==> " + countWord );
+        } catch (FileNotFoundException e){
+            System.err.println("Файл не найден");
+        }
+
+    }
+    public static void wordLengthMax(){
+        try{
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            String LongestWord = "";
+
+            while (scanner.hasNext()){
+                String word = scanner.next();
+                if (word.length() > LongestWord.length()) {
+                    LongestWord = word;
+
+                }
+            }
+            System.out.println("Самое длинное слово из перечисленных ниже: " + LongestWord);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден");
+        }
+    }
+    public static void frequencyWord(){
+        try{
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            Map<String, Integer> freqWord = new TreeMap<>();
+
+
+            while (scanner.hasNext()){
+                String word = scanner.next();
+                freqWord.put(word, freqWord.getOrDefault(word, 0) + 1);
+
+            }
+            System.out.println("В файле встречаются следующие слова:");
+            freqWord.forEach((word, frequency) -> System.out.println( word + " ==> " + frequency + " раз"));
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Файл не найден");
+        }
+    }
+}
